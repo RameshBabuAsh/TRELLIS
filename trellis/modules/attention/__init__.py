1c90a1d72bfda3d4879ba7e17d9de973be8995b7
Ramesh Babu <srameshbabu2004@gmail.com>
2025-01-01 19:04:32 +0530

    Update __init__.py

--- trellis/modules/attention/__init__.py
+++ trellis/modules/attention/__init__.py
@@ -13,7 +13,7 @@ def __from_env():
     env_sttn_debug = os.environ.get('ATTN_DEBUG')
     
     if env_attn_backend is not None and env_attn_backend in ['xformers', 'flash_attn', 'sdpa', 'naive']:
-        BACKEND = 'xformers
+        BACKEND = 'xformers'
     if env_sttn_debug is not None:
         DEBUG = env_sttn_debug == '1'
 
